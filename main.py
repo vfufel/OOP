a82c3ee31f3c10204b8a8edf17473d87ae100b25
Vladislav Bubelev <bubelev.vlad@gmail.com>
2023-10-30 17:56:59 +0300

    Second task

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ class Student:
 
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)
+    
+    def rate_hw(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 
 class Mentor:
@@ -18,6 +27,16 @@ class Mentor:
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
+
+class Lecturer(Mentor):
+
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Reviewer(Mentor):
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -29,23 +48,20 @@ class Mentor:
             return 'Ошибка'
 
 
-class Lecturer(Mentor):
-
-    pass
-
-class Reviewer(Mentor):
-
-    pass
-
-
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
 cool_mentor = Mentor('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_lecturer = Lecturer('Some', 'Buddy')
+cool_lecturer.courses_attached += ['Python']
 
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
+
+best_student.rate_hw(cool_lecturer, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 9)
+
+print(cool_lecturer.grades)
 print(best_student.grades)
